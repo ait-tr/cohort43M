bbd8770789f6b2a487e20b526a8517942ac9a097
Evgenygroup <mailfrom61@gmail.com>
2024-03-05 13:25:15 +0100

    adds lesson 1

---
+++ basic_programming/lesson_01/code/main/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");  // команда вывода на печать. В конце команды ставится ;
+        // тело метода
+    }
+
+    // Class Body  Тело класса
+    // one-line  onjojbkbkhbkhbhb
+
+    // comments
+    //  rtzz
+
+    /* для нескольких строк
+    gggggggggggggg
+    uuuuuuuuuuuuuuu
+    jjjjjjjjjjjjjjjjjjjjjjjjjj
+     */
+    /* fvdnjmuk,*/
+
+
+}
