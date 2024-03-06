0be9810b7574a651c94d9a4e3df5d2257eea7adc
Vladyslav Velykyy <vlad.velykyy@gmail.com>
2024-03-06 20:35:25 +0000

    (HW)-user: 'Vladyslav_Velykyy-vlad.velykyy@gmail.com', commit time: '2024-03-06 20:35:25'

---
+++ basic_programming/lesson_02/hw_solution/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String [] args ) {
+        String a ;
+        a = " Wlad " ;
+        int b ;
+        b = 52 ;
+        String c ;
+        c = " Ukraine " ;
+
+        System.out.println( "Mein Name is" + a );
+        System.out.println( "I am " +b+ " years old");
+        System.out.println( "I am from" + c );
+    }
+}
