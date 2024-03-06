adf6070794a8a84083f4074237bd0dada171721b
Kateryna Ushenko <shkolnidz@gmail.com>
2024-03-06 21:15:21 +0000

    (HW)-user: 'Kateryna_Ushenko-shkolnidz@gmail.com', commit time: '2024-03-06 21:15:21'

---
+++ basic_programming/lesson_02/hw_solution/src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java! ");
+        System.out.println("My name is ");
+        System.out.println("Kateryna");
+
+
+        System.out.print("Hello Java! ");
+        System.out.print("My name is ");
+        System.out.print("Kateryna ");
+
+
+
+
+   int x;
+   x = 10;
+   System.out.println(x);
+   x = 15;
+   System.out.println(x);
+int y = 5;
+x = y + 1 + x;
+x = y;
+System.out.println(x + y);
+
+int sum = x + y;
+System.out.println(sum);
+
+int fifty = 50;
+System.out.println(fifty + "1");
+System.out.println(fifty +1);
+
+System.out.println("variable fifty is " + fifty);
+System.out.println("Sum of 1 and 2 = " + (1 + 2));
+
+var firstName = "Kateryna";
+int thirtyfour = 34;
+x = 34;
+String country = " Ukraine ";
+
+System.out.println("My name is " + firstName);
+System.out.println("I am " + ( x )+ " years old");
+System.out.println("I am from " + country);
+
+    }
+    // komanda vivoda na pechat. V konce stavitsa ;
+}
