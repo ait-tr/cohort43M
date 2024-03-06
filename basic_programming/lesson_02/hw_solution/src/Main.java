518e1ceedfdd4675320e53be1ca8d87da67947f2
Anastassia Markov <mnl040359@gmail.com>
2024-03-06 20:16:24 +0000

    (HW)-user: 'Anastassia_Markov-mnl040359@gmail.com', commit time: '2024-03-06 20:16:24'

---
+++ basic_programming/lesson_02/hw_solution/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String a;
+        a = ("Анастасия");
+        System.out.println("Меня зовут " + a);
+
+        int x = 19;
+        System.out.println("Мне " + x + " лет");
+
+        String b;
+        b = ("Гамбург");
+        System.out.println("Я из " + b);
+
+
+    }
+}
