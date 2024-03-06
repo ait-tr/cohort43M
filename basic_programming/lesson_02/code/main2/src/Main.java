bbd0b4782a2be196f88f82e8d45d5535434e71c3
Evgenygroup <mailfrom61@gmail.com>
2024-03-06 13:17:00 +0100

    adds lesson 2

---
+++ basic_programming/lesson_02/code/main2/src/Main.java
@@ -0,0 +1,114 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello java! ");
+
+        System.out.println("My name is ");
+        System.out.println("Evgeny");
+
+        System.out.print("Hello java! ");
+        System.out.print("My name is ");
+        System.out.print("Evgeny");
+
+        System.out.println();
+
+        System.out.println("My name is " + " Evgeny");
+
+        int x; // обьявление переменной
+
+        x = 10;// присвоение значения
+        System.out.println(x);
+
+        x = 15;
+        System.out.println(x);
+
+        int y = 5;
+
+        x = x + 1; //16
+        x = y + 1 + x; // x = 22
+        x = y;
+      //  y = x;
+     //   System.out.println(x);
+     //   System.out.println(y);
+
+        System.out.println(x + y);
+
+        int sum = x + y;
+        System.out.println(sum);
+
+        int b,c,d;
+        b = c = d = 25;
+
+        System.out.println(50);
+        System.out.println("50");
+
+        int fifty = -50;
+        System.out.println(fifty +"1");
+        System.out.println(fifty + 1);
+
+        System.out.println("variable fifty is " + fifty);
+        System.out.println("Sum of 1 and 2 =" + (1 + 2));
+
+
+
+
+
+
+
+    }
+
+    //bhbbk
+    /* jknljnljnlnljnljn
+       hbbbbbbbbb                     */
+    /*
+    00
+    01
+    10
+    11
+    000  0
+    001  1
+    010  2
+    011  3
+    100  4
+    101  5
+    110  6
+    111  7
+   1000  8
+
+   0-9
+   543
+   345
+
+   5*100 + 4*10 + 3*1
+   5*10^2 +4*10^1 +3*10^0
+
+   0100    2^3*0 + 2^2*1 + 2^1*0 +2^0*0 = 0 + 4*1 + 0 + 0 = 4
+
+
+
+Variables Переменные
+     */
+    int a;
+    String b;
+
+    /*
+    Правила для названий переменных:
+    - может содержать любые буквы и цифры и знак подчеркивания. Но не может начинаться с цифры
+    - не может содержать знаков пунктуации и пробелов
+    - не может быть ключевым словом языка
+    - переменную принято называть с маленькой буквы
+
+     */
+     int sum;
+     int Sum; // так не принято
+     int our_sum; // snake case -  так не принято
+     int oursum;
+     int ourSum; // camel case -  так принято
+
+    int int1;
+
+
+
+
+
+}
