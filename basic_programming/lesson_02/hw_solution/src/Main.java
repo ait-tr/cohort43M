7cf3227aef99966bd0bee3d269812c9340745ec5
Roman Maliarenko <romamalarenko1@gmail.com>
2024-03-06 23:26:18 +0000

    (HW)-user: 'Roman_Maliarenko-romamalarenko1@gmail.com', commit time: '2024-03-06 23:26:18'

---
+++ basic_programming/lesson_02/hw_solution/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args)  {
+int age = 19;
+String name  = "Roman ";
+String town = " Holzkirchen";
+System.out.println("My name is " + name ) ;
+System.out.println("Age " + age );
+System.out.println("Town " + town );
+
+    }
+
+}
