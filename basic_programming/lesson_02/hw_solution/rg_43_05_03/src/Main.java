b461a96eeeca8fbebdd4f25a8d021bec1b726df6
Chaikovsky Taras <taraschaikovskii556@gmail.com>
2024-03-06 21:33:37 +0000

    (HW)-user: 'Chaikovsky_Taras-taraschaikovskii556@gmail.com', commit time: '2024-03-06 21:33:37'

---
+++ basic_programming/lesson_02/hw_solution/rg_43_05_03/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String n;
+        n="Taras";
+        int a=35;
+        String h;
+        h="Ukraine";
+        System.out.println("My name is "+n);
+        System.out.println("I am "+a+" years old");
+        System.out.println("I am from "+h);
+    }
+    }
