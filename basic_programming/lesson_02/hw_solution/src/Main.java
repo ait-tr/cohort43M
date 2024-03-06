c518856bff1bd8ebe8ec4d8ddcf07962898b6837
Alisa Tongaliuk <tongalise@gmail.com>
2024-03-06 21:41:11 +0000

    (HW)-user: 'Alisa_Tongaliuk-tongalise@gmail.com', commit time: '2024-03-06 21:41:11'

---
+++ basic_programming/lesson_02/hw_solution/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Alisa";
+        int age = 24;
+        String country = "UK";
+        System.out.println("My name is " + name);
+        System.out.println("I'm " + age + " years old");
+        System.out.println("I from " + country);
+
+    }
+}
