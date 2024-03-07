ac01391845a93729e45916e55d905653361b454e
Olga Deis <deis666@gmx.de>
2024-03-07 13:09:33 +0000

    (HW)-user: 'Olga_Deis-deis666@gmx.de', commit time: '2024-03-07 13:09:33'

---
+++ basic_programming/lesson_03/hw_solution/HA3Teil2/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 6;
+        int quad = num * num;
+        System.out.println("Квадрат числа " + num + " = " + quad);
+    }
+}
