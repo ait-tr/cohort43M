f6b941c4f0ffca44cc176ae51f3ab5fe7a4a5479
Serhii Shvets <serega.shvets21@gmail.com>
2024-03-07 09:53:15 +0000

    (HW)-user: 'Serhii_Shvets-serega.shvets21@gmail.com', commit time: '2024-03-07 09:53:15'

---
+++ basic_programming/lesson_02/hw_solution/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "John";
+        name = "Sergey";
+
+        int age = 29;
+
+        String city = "Berlin";
+
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("I am from " + city);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
