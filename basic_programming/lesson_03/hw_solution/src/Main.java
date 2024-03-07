f95f9c9679ab76bd8aab87358c9cfaaf11a859de
Vladyslav Velykyy <vlad.velykyy@gmail.com>
2024-03-07 16:59:51 +0000

    (HW)-user: 'Vladyslav_Velykyy-vlad.velykyy@gmail.com', commit time: '2024-03-07 16:59:51'

---
+++ basic_programming/lesson_03/hw_solution/src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Задача 1.
+        //Написать программу, выводящую на экран результат деления двух чисел 25 и 3. Результат не должен быть округлен до целых. Посчитать также остаток от деления этих чисел.
+
+          //      Задача 2.
+        //Написать программу, выводящую на экран значение переменной типа инт в квадрате. Предварительно обьявите эту переменную и задайте ее значение.
+       // ( Возведение в квадрат равносильно умножению числа на себя)
+
+//1
+int x = 25;
+int y = 3;
+int sum = x/y;
+int remainder =x%y;
+
+System.out.println("result is " + sum );
+System.out.println("remainde is " + remainder);
+// 2
+        int a = 25;
+        long sum1 = a*a ;
+        System.out.println(" Qudrat is " + sum1);
+
+        }
+    }
+
+    
