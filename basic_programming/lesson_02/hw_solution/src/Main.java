7a19fdaf03e2b9cd7dd3df60a1051ee8acad10b4
Weimer Katja <katjawmr92@gmail.com>
2024-03-07 12:17:05 +0000

    (HW)-user: 'Weimer_Katja-katjawmr92@gmail.com', commit time: '2024-03-07 12:17:05'

---
+++ basic_programming/lesson_02/hw_solution/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Katja");
+        System.out.println("I am 31 years old");
+        System.out.println("I am from Tomsk");
+    }
+}
