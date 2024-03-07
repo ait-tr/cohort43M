9c9a8a4c19525a60473890f7f7c88f0c6cb2fd01
Alisa Tongaliuk <tongalise@gmail.com>
2024-03-07 14:17:08 +0000

    (HW)-user: 'Alisa_Tongaliuk-tongalise@gmail.com', commit time: '2024-03-07 14:17:08'

---
+++ basic_programming/lesson_03/hw_solution/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 1 task
+
+        System.out.println("25 / 3 = " + 25.0/3);
+        System.out.println("25 % 3 = " + 25.0%3);
+        // 2 task
+        int num = 5;
+//      System.out.println(Math.pow(num, 2));
+        System.out.println(num * num);
+
+    }
+}
