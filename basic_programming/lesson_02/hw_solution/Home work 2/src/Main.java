96a07b2fe1374bb9dfde67092874fa8e828b291a
Alexander Varnavin-Braun <vas681@mail.ru>
2024-03-07 09:20:27 +0000

    (HW)-user: 'Alexander_Varnavin-Braun-vas681@mail.ru', commit time: '2024-03-07 09:20:27'

---
+++ basic_programming/lesson_02/hw_solution/Home work 2/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My name is " + "Alexander");
+        System.out.println("I am 37 years old ");
+        System.out.println("I am from Passau");
+
+        String name = "Alexander";
+        int age = 37;
+        String city = "Passau";
+
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("I am from " + city);
+
+    }
+}
