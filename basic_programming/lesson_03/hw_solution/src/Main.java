b0bdedafd10856629b33aee244ad831a2b17df38
Weimer Katja <katjawmr92@gmail.com>
2024-03-07 14:08:21 +0000

    (HW)-user: 'Weimer_Katja-katjawmr92@gmail.com', commit time: '2024-03-07 14:08:21'

---
+++ basic_programming/lesson_03/hw_solution/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double result = 25.0 / 3.0;
+        System.out.println("25/3 with remainder = " + result);
+
+        int resultOther = 25 % 3;
+        System.out.println("resultOther = " + resultOther);
+
+        int x;
+        x = 25*25;
+        System.out.println("x = " + x);
+
+
+
+    }
+}
