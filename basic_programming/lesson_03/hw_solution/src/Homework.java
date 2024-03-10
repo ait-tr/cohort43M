9f550c0db03dedf9b1d5f24d0efba856053eb976
Roman Maliarenko <romamalarenko1@gmail.com>
2024-03-10 15:17:46 +0000

    (HW)-user: 'Roman_Maliarenko-romamalarenko1@gmail.com', commit time: '2024-03-10 15:17:46'

---
+++ basic_programming/lesson_03/hw_solution/src/Homework.java
@@ -0,0 +1,14 @@
+public class Homework {
+    public static void main(String[] args) {
+        double a = 25;
+        double b = 3;
+        double c = a/b;
+        double x = a%b;
+        System.out.println(c);
+        System.out.println(x);
+
+        int d = 4;
+        int f = d*d;
+        System.out.println(f);
+    }
+}
