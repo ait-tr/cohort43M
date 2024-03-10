99042af311cff581a6c4a8268d47e0f489637c2e
Sahirov Leonid <leosagir1983@gmail.com>
2024-03-10 17:24:53 +0000

    (HW)-user: 'Sahirov_Leonid-leosagir1983@gmail.com', commit time: '2024-03-10 17:24:53'

---
+++ basic_programming/lesson_01/hw_solution/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+String name = "Leonid";
+int age = 40;
+String city = "Mariupol";
+
+ System.out.println("My name is " + name);
+ System.out.println("I am " + age + " years old");
+ System.out.println("I am from city " + city);
+    }
+}
