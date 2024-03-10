90bc7836fef231e2c227bf348ba7f0fb4f2cb31b
Anna Schaedlich <as001963@gmail.com>
2024-03-10 20:09:40 +0000

    (HW)-user: 'Anna_Schaedlich-as001963@gmail.com', commit time: '2024-03-10 20:09:40'

---
+++ basic_programming/lesson_02/hw_solution/src/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+        int x = 47;
+        System.out.println("Меня зовут " + "Анна");
+        System.out.println("Мне " + x + " лет");
+        System.out.println("Я из " + "России");
+        String name = "Anna";
+        String city = "Moscow";
+        int age = x;
+                System.out.println("My name is " + name);
+        System.out.println("I am " + age + " old");
+        System.out.println("I am from " + city);
+
+    }
+}
