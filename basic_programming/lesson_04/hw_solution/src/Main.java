be6912925c0588511041fb1c0b08afe95577bd82
Natalia Boiko <nataliboyko3@gmail.com>
2024-03-11 21:25:07 +0000

    (HW)-user: 'Natalia_Boiko-nataliboyko3@gmail.com', commit time: '2024-03-11 21:25:07'

---
+++ basic_programming/lesson_04/hw_solution/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 7;
+        boolean resalt;
+
+        System.out.println(a + b == 10 );
+        System.out.println( a == 10);
+        System.out.println( b == 10);
+
+
+
+    }
+}
