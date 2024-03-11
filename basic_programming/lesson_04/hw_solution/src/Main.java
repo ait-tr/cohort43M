587881937a94fbae0ce9de91478c31348e82b281
Weimer Katja <katjawmr92@gmail.com>
2024-03-11 17:17:45 +0000

    (HW)-user: 'Weimer_Katja-katjawmr92@gmail.com', commit time: '2024-03-11 17:17:45'

---
+++ basic_programming/lesson_04/hw_solution/src/Main.java
@@ -0,0 +1,32 @@
+import com.sun.source.doctree.SummaryTree;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean res;
+        int a;
+        int b;
+
+        a = 10;
+        b = 0;
+
+        System.out.println(a==10);
+
+        System.out.println(a+b==10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
