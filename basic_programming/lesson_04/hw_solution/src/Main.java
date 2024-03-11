78d0712474397746ca123886a955bbb6bc22638d
Natalia Boiko <nataliboyko3@gmail.com>
2024-03-11 21:37:41 +0000

    (HW)-user: 'Natalia_Boiko-nataliboyko3@gmail.com', commit time: '2024-03-11 21:37:41'

--- basic_programming/lesson_04/hw_solution/src/Main.java
+++ basic_programming/lesson_04/hw_solution/src/Main.java
@@ -2,15 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int a = 3;
-        int b = 7;
-        boolean resalt;
-
-        System.out.println(a + b == 10 );
-        System.out.println( a == 10);
-        System.out.println( b == 10);
+      int a,b;
+      a = 7;
+      b = 3;
+      if ( a == 10 && (a + b) == 10 && b == 10);{
+        System.out.printf("Result = true");}
 
 
 
+        }
     }
-}
