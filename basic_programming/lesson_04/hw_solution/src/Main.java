f9a214a95742f7064092d530f9dc69ad5a91b51f
Olga Deis <deis666@gmx.de>
2024-03-11 12:45:21 +0000

    (HW)-user: 'Olga_Deis-deis666@gmx.de', commit time: '2024-03-11 12:45:21'

---
+++ basic_programming/lesson_04/hw_solution/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+        boolean res = (a == 10) || (b == 10) || (a + b == 10);
+        System.out.println(res);
+    }
+}
