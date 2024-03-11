0382248f07660e75bbb76075ec5ffe5804364b60
Serhii Shvets <serega.shvets21@gmail.com>
2024-03-11 09:23:33 +0000

    (HW)-user: 'Serhii_Shvets-serega.shvets21@gmail.com', commit time: '2024-03-11 09:23:33'

---
+++ basic_programming/lesson_03/hw_solution/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double result = 25.0 / 3;
+        System.out.println(result);
+
+
+        int remainder = 25 % 3;
+        System.out.println(remainder);
+
+        int n = 4;
+        System.out.println(n * n);
+    }
+}
