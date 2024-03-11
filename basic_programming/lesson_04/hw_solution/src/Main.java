c40d7e2de3a6f8aeeb9b175157e8e8df5b334d88
Anastassia Markov <mnl040359@gmail.com>
2024-03-11 14:14:43 +0000

    (HW)-user: 'Anastassia_Markov-mnl040359@gmail.com', commit time: '2024-03-11 14:14:43'

---
+++ basic_programming/lesson_04/hw_solution/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Пусть есть две переменные целого типа a и b.
+        Используя логические операторы написать выражение, результат которого
+        будет true если значение хотя бы одной из этих переменных равно 10 или
+        их сумма равна 10.
+         */
+        int a = 7;
+        int b = 3;
+        boolean result;
+        result = a != b;
+        System.out.println(result);
+    }
+}
