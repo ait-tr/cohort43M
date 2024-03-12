58be127290fa5a890ce3328633d208b5f7205e38
Weimer Katja <katjawmr92@gmail.com>
2024-03-12 14:38:25 +0000

    (HW)-user: 'Weimer_Katja-katjawmr92@gmail.com', commit time: '2024-03-12 14:38:25'

---
+++ basic_programming/lesson_05/hw_solution/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner ourScanner = new Scanner(System.in);
+        String firstName = ourScanner.nextLine();
+        String lastName = ourScanner.nextLine();
+        int age = ourScanner.nextInt();
+
+        System.out.println("The name is " + firstName + " " + lastName + " ,age is " + age);
+
+
+
+
+        // Задача 2
+
+        char ch;
+        ch = 65;
+        System.out.println("Char now is " + ch);
+    }
+}
