92bccf8294d909b9b2e8c9eb4232fdc2b2d1e236
Roman Maliarenko <romamalarenko1@gmail.com>
2024-03-12 02:18:44 +0000

    (HW)-user: 'Roman_Maliarenko-romamalarenko1@gmail.com', commit time: '2024-03-12 02:18:44'

---
+++ basic_programming/lesson_04/hw_solution/src/homework4.java
@@ -0,0 +1,18 @@
+public class homework4 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        if (a==10 | b==10 | a+b==10 ){
+            System.out.println(true);
+        }
+        //другой вариант
+        boolean f = a==10;
+        boolean g = b==10;
+        boolean h = a + b ==10;
+        System.out.println(h);
+        System.out.println(g);
+        System.out.println(f);
+        
+    }
+}
+
