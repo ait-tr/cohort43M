a83c8f15c948f4137bfd4f113b156dd3ef72dea0
Serhii Shvets <serega.shvets21@gmail.com>
2024-03-12 08:53:23 +0000

    (HW)-user: 'Serhii_Shvets-serega.shvets21@gmail.com', commit time: '2024-03-12 08:53:23'

---
+++ basic_programming/lesson_04/hw_solution/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a, b;
+        a = 10;
+        b = 0;
+        if ((a + b) == 10 || a == 10 || b == 10) {
+            System.out.println("Result " + true);
+
+        }
+    }
+
+}
