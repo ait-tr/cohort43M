984da60d54bc89df3a3ff7fb917aa35317247c23
Sahirov Leonid <leosagir1983@gmail.com>
2024-03-12 08:12:57 +0000

    (HW)-user: 'Sahirov_Leonid-leosagir1983@gmail.com', commit time: '2024-03-12 08:12:57'

---
+++ basic_programming/lesson_04/hw_solution/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=3;
+        int b=4;
+        boolean result=(a==10) || (b==10) || (a+b==10);
+        System.out.println("Результат 1: " + result);
+
+        int c=4;
+        int d=6;
+        boolean result2=(c==10) || (d==10) || (c+d==10);
+        System.out.println("Результат 2: " + result2);
+    }
+}
