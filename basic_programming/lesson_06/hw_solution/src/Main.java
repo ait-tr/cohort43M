b34755601c35b40050a1d9cdca3930133e7afd58
Olga Deis <deis666@gmx.de>
2024-03-13 16:18:02 +0000

    (HW)-user: 'Olga_Deis-deis666@gmx.de', commit time: '2024-03-13 16:18:02'

---
+++ basic_programming/lesson_06/hw_solution/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int firstNumber = 2;
+        int secondNumber = 2;
+        int sum = firstNumber + secondNumber;
+
+        if (firstNumber == secondNumber) {
+            sum *= 2;
+        }
+
+        System.out.println("Sum of the numbers: " + sum);
+    }
+}
