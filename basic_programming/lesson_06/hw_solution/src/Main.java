52c108b6ba4353cc4b04bf07e1578f4c47a10f71
Olga Deis <deis666@gmx.de>
2024-03-13 16:20:29 +0000

    (HW)-user: 'Olga_Deis-deis666@gmx.de', commit time: '2024-03-13 16:20:29'

--- basic_programming/lesson_06/hw_solution/src/Main.java
+++ basic_programming/lesson_06/hw_solution/src/Main.java
@@ -1,15 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 7;
-        int num3 = 5;
+        int firstNumber = 2;
+        int secondNumber = 2;
+        int sum = firstNumber + secondNumber;
 
-        if (num1 == num2 && num2 == num3) {
-            System.out.println("All numbers are equal.");
-        } else if (num1 != num2 && num2 != num3 && num1 != num3) {
-            System.out.println("All numbers are different.");
-        } else {
-            System.out.println("Some numbers are equal.");
+        if (firstNumber == secondNumber) {
+            sum *= 2;
         }
+
+        System.out.println("Sum of the numbers: " + sum);
     }
 }
