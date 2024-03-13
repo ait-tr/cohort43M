354caa2e56d5b50032f00734307cbe587f905f65
Serhii Shvets <serega.shvets21@gmail.com>
2024-03-13 08:42:24 +0000

    (HW)-user: 'Serhii_Shvets-serega.shvets21@gmail.com', commit time: '2024-03-13 08:42:24'

--- basic_programming/lesson_05/hw_solution/src/Main2.java
+++ basic_programming/lesson_05/hw_solution/src/Main2.java
@@ -1,9 +1,8 @@
 public class Main2 {
     public static void main(String[] args) {
 
-        var b2 = 65;
-        char ch;
-        ch = (char) b2;
-        System.out.println("Char: " + ch);
+        char d1 = '&';
+        int f = d1;
+        System.out.println("Char: " + f);
     }
 }
