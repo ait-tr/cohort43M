d9e9378dec0a8c5bf5c5971d77744bbf1e93250d
Natalia Boiko <nataliboyko3@gmail.com>
2024-03-14 19:22:26 +0000

    (HW)-user: 'Natalia_Boiko-nataliboyko3@gmail.com', commit time: '2024-03-14 19:22:26'

---
+++ basic_programming/lesson_07/hw_solution/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+      int res = sum( 15,8);
+       System.out.println("Result = " + res);
+
+    }
+    public static int sum(int a,int b) {
+        int res = a + b;
+        return res;
+
+    }
+
+
+}
