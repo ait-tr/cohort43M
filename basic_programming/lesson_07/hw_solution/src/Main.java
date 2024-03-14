032b74fc28b14fe26142801a17ef5c4f3ff2b5eb
Natalia Boiko <nataliboyko3@gmail.com>
2024-03-14 19:38:59 +0000

    (HW)-user: 'Natalia_Boiko-nataliboyko3@gmail.com', commit time: '2024-03-14 19:38:59'

--- basic_programming/lesson_07/hw_solution/src/Main.java
+++ basic_programming/lesson_07/hw_solution/src/Main.java
@@ -2,15 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-      int res = sum( 15,8);
-       System.out.println("Result = " + res);
-
-    }
-    public static int sum(int a,int b) {
-        int res = a + b;
-        return res;
+        double resalt = gradCels(10);
+        System.out.println("Temperatura Celsii = " + resalt);
 
     }
 
+    public static double gradCels(double f) {
+        double gradcels1 = 5 * (f - 32) / 9;
+        return gradcels1;
+    }
+        }
 
-}
+
