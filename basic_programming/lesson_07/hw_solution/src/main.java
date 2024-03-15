75580ffe4550ce7d40f5825f4857973e20edb7d4
Sahirov Leonid <leosagir1983@gmail.com>
2024-03-15 08:12:10 +0000

    (HW)-user: 'Sahirov_Leonid-leosagir1983@gmail.com', commit time: '2024-03-15 08:12:10'

--- basic_programming/lesson_07/hw_solution/src/main.java
+++ basic_programming/lesson_07/hw_solution/src/main.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
 public class main {
-    public static int sum(int num1, int num2) {
-        return num1 + num2;
-    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Imput number 1: ");
-        int num1 = scanner.nextInt();
-        System.out.print("Imput number 2: ");
-        int num2 = scanner.nextInt();
-        System.out.println("SUMMA: " + sum(num1, num2));
+        System.out.print("Temperatur Farenheit: ");
+        double f = scanner.nextDouble();
+        System.out.println("Temperatur in Celcium: " + farenheitToCelcius(f));
+    }
+
+    public static double farenheitToCelcius(double f) {
+        return 5 * (f - 32) / 9;
     }
 }
