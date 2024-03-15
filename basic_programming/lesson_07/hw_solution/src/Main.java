ff9f1642c2c3292760dd19e5d03bb08ef721fec5
Olga Deis <deis666@gmx.de>
2024-03-15 06:38:25 +0000

    (HW)-user: 'Olga_Deis-deis666@gmx.de', commit time: '2024-03-15 06:38:25'

---
+++ basic_programming/lesson_07/hw_solution/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int result = sum(num1, num2);
+        System.out.println("Сумма чисел " + num1 + " и " + num2 + " равна " + result);
+    }
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
