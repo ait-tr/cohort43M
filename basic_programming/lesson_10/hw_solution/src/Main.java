55c2ab1b5efe05296c3a26815deca7ea50391d2f
Anna Schaedlich <as001963@gmail.com>
2024-03-19 19:43:58 +0000

    (HW)-user: 'Anna_Schaedlich-as001963@gmail.com', commit time: '2024-03-19 19:43:58'

--- basic_programming/lesson_10/hw_solution/src/Main.java
+++ basic_programming/lesson_10/hw_solution/src/Main.java
@@ -3,24 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Task 1
+        //Написать метод, принимающий с клавиатуры положительное число и печатающий
+        // затем таблицу умножения для этого числа ( т.е. печатающий произведение
+        // этого числа на все числа от 1 до 10).
+        //Использовать в решении цикл for.
         Scanner scanner = new Scanner(System.in);
         System.out.println(" Enter a number from 1 to 10: ");
         int num = scanner.nextInt();
         printTable(num);
 
         // Task 2
+        //Переписать задачу про треугольник из сегодняшнего занятия используя циклы while
         printTriangle(6);
     }
 
 
     public static void printTable(int i) {
+        for(int j = 1; j <=10; j++)
 
 
-            int j = 1;
-            while (j <=10) {
+            //int j = 1;
+            //while (j <=10) {
                 System.out.println(i + " * " + j + " = " + i * j);
-                j++;
-            }
+               // j++;
+            //}
             System.out.println("**************");
 
 
