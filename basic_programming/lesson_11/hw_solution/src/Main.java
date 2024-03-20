f795e795610296c88d9e9031d70b21fa3aca9cfb
Weimer Katja <katjawmr92@gmail.com>
2024-03-20 16:00:11 +0000

    (HW)-user: 'Weimer_Katja-katjawmr92@gmail.com', commit time: '2024-03-20 16:00:11'

---
+++ basic_programming/lesson_11/hw_solution/src/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+
+    /*
+-Cоздать массив строк fruits (фрукты) из 4 элементов: Orange,Apple,Banana,Mango.
+-Вывести созданный массив на экран используя цикл for.
+-Заменить в созданном массиве значение первого элемента на Kiwi (т.е. присвоить первому элементу значение Kiwi)
+-Вывести измененный массив на экран используя цикл for.
+Задание можно выполнить непосредственно в методе main, не создавая отдельный метод.
+     */
+    public static void main(String[] args) {
+
+
+        String[] fruits = {"Orange", "Apple", "Banana", "Mango"};
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println("fruits[" + i + "] = " + fruits[i]);
+        }
+
+        fruits[0] = "Kiwi";
+        System.out.println("With Kiwi");
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println("fruits[" + i + "] = " + fruits[i]);
+        }
+
+        System.out.println("Print min");
+        int[] array = {35,9,56,7};
+        findMin(array);
+    }
+
+    /*
+    Дан массив целых чисел. Написать метод, возвращающий наименьший элемент массива.
+public static int findMin(int[] ints)
+     */
+
+    public static int findMin(int[] ints) {
+
+        int min = ints[0];
+
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] < min) {
+                min = ints[i];
+
+
+            }
+        }
+        System.out.println(min);
+        return min;
+    }
+ }
+
