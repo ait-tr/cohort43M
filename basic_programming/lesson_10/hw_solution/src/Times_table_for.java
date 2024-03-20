45465559e174d68e8704c894fe17bdc3a69a9334
Varvara Gaidukova <barbara_phoenix@outlook.com>
2024-03-20 17:16:55 +0000

    (HW)-user: 'Varvara_Gaidukova-barbara_phoenix@outlook.com', commit time: '2024-03-20 17:16:55'

--- basic_programming/lesson_10/hw_solution/src/Times_table_for.java
+++ basic_programming/lesson_10/hw_solution/src/Times_table_for.java
@@ -2,27 +2,32 @@ import java.util.Scanner;
 
 public class Times_table_for {
     public static void main(String[] args) {
-
-      //  Scanner ourScanner = new Scanner(System.in);
-
-       // System.out.println("Enter a number from 1 to 10");
-      //  int i = ourScanner.nextInt();
-
-        for(int i = 1; i <= 10; i++) {
-
-            for (int j = 1; j <= 10; j++) {
-                System.out.println(i + " * " + j + " = " + i * j);
+    printTableForNumber();
             }
-            System.out.println();
+            public static void printTableForNumber()
+            {
+               Scanner sc = new Scanner(System.in);
+
+               int num;
+
+               System.out.println("Enter a number");
+
+               num = sc.nextInt();
+
+               System.out.println("Multiplication table of " + num);
+
+               for ( int i = 1; i <= 10; i++)
+               {
+                   System.out.println(num + " * " + i + " = " + (num * i));
+               }
+            }
 
         }
-    }
-}
 
 /*
 Задача 1
 Написать метод, принимающий с клавиатуры положительное число и печатающий затем
 таблицу умножения для этого числа ( т.е. печатающий произведение этого числа
- на все числа от 1 до 10).
+ на все числа от 1 до 9).
 Использовать в решении цикл for.
  */
