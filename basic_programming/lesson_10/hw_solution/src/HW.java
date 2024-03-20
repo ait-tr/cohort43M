2012f69168c1414562623833aee4474c810804a7
Serhii Shvets <serega.shvets21@gmail.com>
2024-03-20 20:39:42 +0000

    (HW)-user: 'Serhii_Shvets-serega.shvets21@gmail.com', commit time: '2024-03-20 20:39:42'

---
+++ basic_programming/lesson_10/hw_solution/src/HW.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class HW {
+    /*
+    Задача 1
+Написать метод, принимающий с клавиатуры положительное число и печатающий затем таблицу умножения для этого числа
+ ( т.е. печатающий произведение этого числа на все числа от 1 до 10). Использовать в решении цикл for.
+     */
+
+    public static void main(String[] args) {
+        printTable();
+        printTriangle(5);
+        printTriangleWhile(5);
+    }
+
+    public static void printTable () {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        System.out.println("Enter a number");
+
+        num = sc.nextInt();
+        System.out.println("Multiplication table of "  + num);
+
+        for(int i = 1; i <= 10; i++ ) {
+            System.out.println(num + "*" + i + " = " + (num*i));
+        }
+    }
+
+    /*
+    Задача 2
+Переписать задачу про треугольник из сегодняшнего занятия используя циклы while
+     */
+
+    public static void printTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printTriangleWhile(int n) {
+        int i = 1;
+        while (i <= n) {
+            int j = 1;
+            while (j <= i) {
+                System.out.print(j);
+                j++;
+            }
+            i++;
+            System.out.println();
+        }
+    }
+}
+
