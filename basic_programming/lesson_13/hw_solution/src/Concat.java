cc5e0411b3ee276b78c34b3df749f9bd07db1231
Varvara Gaidukova <barbara_phoenix@outlook.com>
2024-03-23 07:51:45 +0000

    (HW)-user: 'Varvara_Gaidukova-barbara_phoenix@outlook.com', commit time: '2024-03-23 07:51:45'

---
+++ basic_programming/lesson_13/hw_solution/src/Concat.java
@@ -0,0 +1,17 @@
+public class Concat {
+    public static void main(String[] args) {
+    String strOne = "Hello";
+    String strTwo = "There";
+    String result = strOne.concat(strTwo);
+
+    System.out.println(result.toUpperCase());
+    }
+}
+
+/*
+Задача 2
+Написать метод, принимающий две строки и возвращающий эти строки,
+соединенные вместе и в верхнем регистре.
+Пример: "Hello", "There" -> HELLOTHERE
+
+ */
---
+++ basic_programming/lesson_13/hw_solution/src/String_length.java
@@ -0,0 +1,15 @@
+public class String_length {
+    public static void main(String[] args) {
+        String java = "Hello";
+        System.out.println(java.length());
+    }
+
+
+}
+
+/*
+Задача 1
+Написать метод, принимающий строку и возвращающий целое число, равное длине строки
+Пример: Hello -> 5
+
+ */
