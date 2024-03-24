1af5213aa33131b858183063d79f47853c57a8ed
Roman Maliarenko <romamalarenko1@gmail.com>
2024-03-24 22:59:46 +0000

    (HW)-user: 'Roman_Maliarenko-romamalarenko1@gmail.com', commit time: '2024-03-24 22:59:46'

---
+++ basic_programming/lesson_13/hw_solution/src/homework.java
@@ -0,0 +1,20 @@
+public class homework {
+    public static void main(String[] args) {
+        String myString = "Hello";
+        int x = string(myString);
+        System.out.println(x);
+        String c = "hello";
+        String f = "world";
+        String upper1 = upperCase(c,f);
+        System.out.println(upper1);
+    }
+    public static int string(String str){
+        return str.length();
+    }
+    public static String upperCase(String str, String str1) {
+        String casse = str + str1;
+        return casse.toUpperCase();
+    }
+
+
+}
