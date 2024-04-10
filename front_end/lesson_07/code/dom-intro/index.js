6744a9384fd3c2346e68ab112df1749fe4a5474b
MIKHAIL <142326174+Hypericum90@users.noreply.github.com>
2024-04-10 15:12:20 +0400

    add files

---
+++ front_end/lesson_07/code/dom-intro/index.js
@@ -0,0 +1,22 @@
+console.log("welcome to js");
+
+// Как создать переменную
+
+let name = "Anna";
+name = 33;
+// console.log(name)
+let x;
+x = Boolean;
+const age = 55;
+// age = 33;
+console.log(age);
+const age2 =age;
+console.log(age2);
+
+const boxElement = document.getElementById("box-1");
+console.log(boxElement);
+
+boxElement.style.color = 'green';
+boxElement.style.backgroundColor = "red"
+boxElement.style.width = "500px"
+boxElement.textContent = "HELLO"
