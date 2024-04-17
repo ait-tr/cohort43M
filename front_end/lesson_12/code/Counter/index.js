1d232027a686668808dca77fff8bcf0f6ca3f646
MIKHAIL <142326174+Hypericum90@users.noreply.github.com>
2024-04-17 15:38:03 +0400

    add files

---
+++ front_end/lesson_12/code/Counter/index.js
@@ -0,0 +1,19 @@
+const plusBtnElement = document.getElementById("plus");
+const minusBtnElement = document.getElementById("minus");
+const counterElement = document.getElementById("counter");
+
+let counter = 0;
+
+const plusHandler = ()=>{
+    counter+=10;
+    counterElement.innerText=counter // увеличиваем значение счетчика на +1
+}
+
+const minusHandler = ()=>{
+    counter-=10;
+    counterElement.innerText=counter // уменьшаем значение счетчика на -1
+}
+
+plusBtnElement.addEventListener('click',plusHandler);
+minusBtnElement.addEventListener('click',minusHandler)
+
