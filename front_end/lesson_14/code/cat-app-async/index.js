0d5e3c128b622000aeb059bcb67bca7cacb5eba0
MIKHAIL <142326174+Hypericum90@users.noreply.github.com>
2024-04-22 01:05:44 +0400

    add files

---
+++ front_end/lesson_14/code/cat-app-async/index.js
@@ -0,0 +1,11 @@
+const catFactsElement = document.getElementById("cat-facts")
+
+//Функцию обертка 
+async function loadCatFact(){
+    const responce = await fetch("https://catfact.ninja/fact")
+    const obj = await responce.json();
+    console.log(obj);
+    const {fact, length}=obj;
+    catFactsElement.textContent= `${fact} ${length}`;
+}
+loadCatFact()
