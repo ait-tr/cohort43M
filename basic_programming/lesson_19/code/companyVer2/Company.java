4a8b56c4f450233efb59db83f22d854a890bdf8d
StanislavPB <perminovsb@gmail.com>
2024-05-06 09:19:19 +0100

    lesson 19

--- basic_programming/lesson_19/code/companyVer2/Company.java
+++ basic_programming/lesson_19/code/companyVer2/Company.java
@@ -3,5 +3,26 @@ package code.companyVer2;
 public class Company {
     public static void main(String[] args) {
         Department firstDepartment = new Department("IT");
+
+
+        Employee[] employees = firstDepartment.getEmployees();
+
+        employees[0] = new Employee(1, "Roman");
+        employees[1] = new Employee(2, "Petr");
+        employees[2] = new Employee(3, "Sergej");
+
+        printCompanyData(firstDepartment);
+
+
+
+    }
+
+    public static void printCompanyData(Department department){
+        System.out.println("Отдел: " + department.getName());
+        for (int i = 0; i < department.getEmployees().length; i++) {
+            System.out.println("Сотрудник: ");
+            System.out.println("Имя: " + department.getEmployees()[i].getName());
+
+        }
     }
 }
