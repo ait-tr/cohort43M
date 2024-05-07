5893631fce49aebe5bad81c2ce5bd51c458f2d4f
StanislavPB <perminovsb@gmail.com>
2024-05-07 14:42:30 +0100

    consultation 07

---
+++ basic_programming/consultation/consultation_07/code/Author.java
@@ -0,0 +1,21 @@
+package code;
+
+public class Author {
+
+    private String authorName;
+
+    public Author(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "authorName='" + authorName + '\'' +
+                '}';
+    }
+}
