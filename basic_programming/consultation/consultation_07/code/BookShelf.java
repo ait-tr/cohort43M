4ed1f394f0b535de9726ba44276f4bfc7ed3478e
StanislavPB <perminovsb@gmail.com>
2024-05-07 14:49:26 +0100

    consultation 07

---
+++ basic_programming/consultation/consultation_07/code/BookShelf.java
@@ -0,0 +1,30 @@
+package code;
+
+import java.util.Arrays;
+
+public class BookShelf {
+
+    private String idNumber;
+    private Book[] books;
+
+    public BookShelf(String idNumber, Book[] books) {
+        this.idNumber = idNumber;
+        this.books = books;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public Book[] getBooks() {
+        return books;
+    }
+
+    @Override
+    public String toString() {
+        return "BookShelf{" +
+                "idNumber='" + idNumber + '\'' +
+                ", books=" + Arrays.toString(books) +
+                '}';
+    }
+}
---
+++ basic_programming/consultation/consultation_07/code/BookStorage.java
@@ -0,0 +1,32 @@
+package code;
+
+import java.util.Arrays;
+
+public class BookStorage {
+
+    private String lineId;
+    private int numberOfShelves;
+    private BookShelf[] bookShelves;
+
+    public BookStorage(String lineId, int numberOfShelves) {
+        this.lineId = lineId;
+        this.numberOfShelves = numberOfShelves;
+        this.bookShelves = new BookShelf[numberOfShelves];
+    }
+
+    public String getLineId() {
+        return lineId;
+    }
+
+    public BookShelf[] getBookShelves() {
+        return bookShelves;
+    }
+
+    @Override
+    public String toString() {
+        return "BookStorage{" +
+                "lineId='" + lineId + '\'' +
+                ", bookShelves=" + Arrays.toString(bookShelves) +
+                '}';
+    }
+}
