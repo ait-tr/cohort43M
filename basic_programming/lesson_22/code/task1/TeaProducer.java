b39f42d3cc732b7e23b8ff5de5dab30dfcd9ddd4
StanislavPB <perminovsb@gmail.com>
2024-05-10 08:53:38 +0100

    lesson 22

---
+++ basic_programming/lesson_22/code/task1/TeaProducer.java
@@ -0,0 +1,21 @@
+package code.task1;
+
+public class TeaProducer {
+
+    private String producerName;
+
+    public TeaProducer(String producerName) {
+        this.producerName = producerName;
+    }
+
+    public String getProducerName() {
+        return producerName;
+    }
+
+    @Override
+    public String toString() {
+        return "TeaProducer{" +
+                "producerName='" + producerName + '\'' +
+                '}';
+    }
+}
---
+++ basic_programming/lesson_22/code/task1/TeaTypes.java
@@ -0,0 +1,19 @@
+package code.task1;
+
+import java.util.Arrays;
+
+public class TeaTypes {
+
+    private String[] teaTypes = {"black","green", "herbal", "fruits"};
+
+    public String[] getTeaTypes() {
+        return teaTypes;
+    }
+
+    @Override
+    public String toString() {
+        return "TeaTypes{" +
+                "teaTypes=" + Arrays.toString(teaTypes) +
+                '}';
+    }
+}
