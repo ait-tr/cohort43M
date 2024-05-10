f12a07eefa6a88b1dad048e39a21a5e2fb4d2923
StanislavPB <perminovsb@gmail.com>
2024-05-10 09:42:00 +0100

    lesson 22

--- basic_programming/lesson_22/code/task1/TeaApp.java
+++ basic_programming/lesson_22/code/task1/TeaApp.java
@@ -4,7 +4,7 @@ public class TeaApp {
     public static void main(String[] args) {
 
         TeaTypes teaTypes = new TeaTypes();
-        TeaCollection teaCollection = new TeaCollection(10);
+        TeaCollection teaCollection = new TeaCollection(6);
 
         teaCollection.printCollection();
 
@@ -16,11 +16,41 @@ public class TeaApp {
 
 
         Tea tea1 = new Tea(1,teaProducer1, "Limon", teaTypes, 0);
-        teaCollection.add(tea1);
+        Tea tea2 = new Tea(2,teaProducer2, "Melon", teaTypes, 2);
+        Tea tea3 = new Tea(3,teaProducer3, "Black Earl Grey", teaTypes, 0);
+        Tea tea4 = new Tea(4,teaProducer4, "Herbal", teaTypes, 2);
+        Tea tea5 = new Tea(5,teaProducer5, "Green + Mint", teaTypes, 1);
+        Tea tea6 = new Tea(6,teaProducer2, "Japan Cherry", teaTypes, 3);
+        Tea tea7 = new Tea(7,teaProducer4, "Carcade", teaTypes, 3);
+        Tea tea8 = new Tea(8,teaProducer1, "Green Jasmine", teaTypes, 1);
+
+        if (teaCollection.add(tea1) >= 0) {
+            System.out.println("Чай успешно добавлен");
+        } else {
+            System.out.println("Добавить не удалось - нет свободного места");
+        } ;
+
+
+        teaCollection.add(tea2);
+        teaCollection.add(tea3);
+        teaCollection.add(tea4);
+        teaCollection.add(tea5);
+        teaCollection.add(tea6);
+        teaCollection.add(tea7);
+
+        if (teaCollection.add(tea8) >= 0) {
+            System.out.println("Чай успешно добавлен");
+        } else {
+            System.out.println("Добавить не удалось - нет свободного места");
+        } ;
 
 
         teaCollection.printCollection();
 
+
+        int teaSearchIndex = 3;
+
+        System.out.println("Чай но индексу: " + teaSearchIndex + " : " + teaCollection.findById(teaSearchIndex));
 
     }
 }
