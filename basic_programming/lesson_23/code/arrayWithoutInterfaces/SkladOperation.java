59d5c904c3df2ffeb6c07d0e0d239181e070da6e
StanislavPB <perminovsb@gmail.com>
2024-05-13 11:54:37 +0100

    lesson 23

--- basic_programming/lesson_23/code/arrayWithoutInterfaces/SkladOperation.java
+++ basic_programming/lesson_23/code/arrayWithoutInterfaces/SkladOperation.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 public class SkladOperation {
 
     public void operations(UserInput ui, Sklad sklad) {
+
         sklad.fillSklad();
         String searchName = ui.inputText("Введите название для поиска: ");
         System.out.println(Arrays.toString(sklad.findByName(searchName)));
