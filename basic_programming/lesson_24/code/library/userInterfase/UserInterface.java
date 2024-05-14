a914850458e852c507ba156ecfb56d91d3fabcaa
StanislavPB <perminovsb@gmail.com>
2024-05-14 14:54:43 +0100

    lesson 24

--- basic_programming/lesson_24/code/library/userInterfase/UserInterface.java
+++ basic_programming/lesson_24/code/library/userInterfase/UserInterface.java
@@ -1,32 +1,95 @@
 package code.library.userInterfase;
 
-import code.library.service.BookService;
-import code.library.service.MagazineService;
+import code.library.entity.Book;
+import code.library.entity.Reader;
 import code.library.var1.UserInput;
+
+import java.util.Arrays;
 
 public class UserInterface {
 
-    UserInput userInput;
+    private UserInterfaceService service;
+    private UserInput userInput;
 
-    private BookService bookService;
-    private MagazineService magazineService;
-
-    public UserInterface(UserInput userInput, BookService bookService, MagazineService magazineService) {
+    public UserInterface(UserInterfaceService service, UserInput userInput) {
+        this.service = service;
         this.userInput = userInput;
-        this.bookService = bookService;
-        this.magazineService = magazineService;
     }
 
     public void start(){
         while (true) {
+            service.printMenu();
+
+            int choice = userInput.inputInt("Enter your choice: ");
+
+            switch (choice) {
+                case 1:
+                    boolean addBookResult = service.addBook();
+
+                    if (addBookResult) {
+                        System.out.println("Книга добавлена успешно");
+                    } else {
+                        System.out.println("Ошибка добавления!");
+                    }
+                    break;
+
+                case 2:
+                    boolean addReaderResult = service.addReader();
+                    if (addReaderResult) {
+                        System.out.println("Читатель добавлен успешно");
+                    } else {
+                        System.out.println("Ошибка добавления!");
+                    }
+                    break;
+
+
+                case 3:
+                    Book foundedBook = service.findBookByTitle();
+                    if (foundedBook != null) {
+                        System.out.println("Книга найдена.");
+                        System.out.println(foundedBook);
+                    } else {
+                        System.out.println("Книги с таким названием нет!");
+                    }
+                    break;
+
+                case 4:
+                    Book[] foundedBooks = service.findBookByAuthor();
+                    if (foundedBooks.length > 0) {
+                        System.out.println("Книги найдены.");
+                        System.out.println(Arrays.toString(foundedBooks));
+                    } else {
+                        System.out.println("Книги такого автора не найдены!");
+                    }
+                    break;
+
+                case 5:
+                    Reader foundedReader = service.findReaderById();
+                    if (foundedReader != null) {
+                        System.out.println("Читатель найден.");
+                        System.out.println(foundedReader);
+                    } else {
+                        System.out.println("Читателя с таким id нет!");
+                    }
+                    break;
+
+                case 7:
+                    System.out.println("Exit ...");
+                    return;
+
+            }
 
         }
     }
 
-
-    public static void printMenu(){
-        System.out.println("======= MENU ========");
-        System.out.println("1. Add book");
-        System.out.println("2. ");
-    }
+//    System.out.println("======= MENU ========");
+//        System.out.println("1. Add book");
+//        System.out.println("2. Add reader");
+//        System.out.println("3. Find book by Title");
+//        System.out.println("4. Find books by Author");
+//        System.out.println("5. Find reader by Id");
+//        System.out.println("6. Lend book to reader");
+//        System.out.println("7. Exit");
+//
+//
 }
---
+++ basic_programming/lesson_24/code/library/userInterfase/UserInterfaceService.java
@@ -0,0 +1,72 @@
+package code.library.userInterfase;
+
+import code.library.entity.Book;
+import code.library.entity.Reader;
+import code.library.service.BookService;
+import code.library.service.MagazineService;
+import code.library.service.ReaderService;
+import code.library.var1.UserInput;
+
+public class UserInterfaceService {
+
+    UserInput userInput;
+
+    private BookService bookService;
+    private MagazineService magazineService;
+    private ReaderService readerService;
+
+    public UserInterfaceService(UserInput userInput, BookService bookService, MagazineService magazineService, ReaderService readerService) {
+        this.userInput = userInput;
+        this.bookService = bookService;
+        this.magazineService = magazineService;
+        this.readerService = readerService;
+    }
+
+    public  void printMenu(){
+        System.out.println("======= MENU ========");
+        System.out.println("1. Add book");
+        System.out.println("2. Add reader");
+        System.out.println("3. Find book by Title");
+        System.out.println("4. Find books by Author");
+        System.out.println("5. Find reader by Id");
+        System.out.println("6. Lend book to reader");
+        System.out.println("7. Exit");
+
+    }
+
+    public boolean addBook() {
+        int id = userInput.inputInt("Enter id: ");
+        String title = userInput.inputText("Enter title: ");
+        String author = userInput.inputText("Enter author: ");
+        String genre = userInput.inputText("Enter genre: ");
+
+        Book newBook = new Book(id, title, author, genre);
+
+        return bookService.addBook(newBook);
+
+    }
+
+    public  boolean addReader() {
+        int id = userInput.inputInt("Enter id: ");
+        String name = userInput.inputText("Enter name: ");
+        return readerService.addReader(new Reader(id, name));
+    }
+
+    public Book findBookByTitle() {
+        String title = userInput.inputText("Enter title for search : ");
+        Book foundedBook = bookService.findByTitle(title);
+        return foundedBook;
+    }
+
+    public Book[] findBookByAuthor() {
+        String author = userInput.inputText("Enter author for search : ");
+        Book[] foundedBooks = bookService.findByAuthor(author);
+        return foundedBooks;
+    }
+
+    public Reader findReaderById() {
+        int readerId = userInput.inputInt("Enter reader id for search : ");
+        Reader foundedReader = readerService.findById(readerId);
+        return foundedReader;
+    }
+}
