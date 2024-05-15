395a05a96f3324f4ff3253b448689659b2fa7985
StanislavPB <perminovsb@gmail.com>
2024-05-15 10:58:11 +0100

    lesson 25

--- basic_programming/lesson_25/code/arrayListPrctice/TvCollection.java
+++ basic_programming/lesson_25/code/arrayListPrctice/TvCollection.java
@@ -1,5 +1,7 @@
 package code.arrayListPrctice;
 
+import java.util.ArrayList;
+
 public class TvCollection {
     public static void main(String[] args) {
         /*
@@ -13,5 +15,36 @@ public class TvCollection {
         удалить один элемент - первый из всех у кого совпадает производитель
 
          */
+
+        ArrayList<TV> tvs = new ArrayList<>();
+
+        tvs.add(new TV("Sony", 55));
+        tvs.add(new TV("Samsung", 65));
+        tvs.add(new TV("Philips", 50));
+        tvs.add(new TV("LG", 60));
+        tvs.add(new TV("Philips", 42));
+
+        for (int i = 0; i < tvs.size(); i++) {
+            System.out.println(tvs.get(i));
+        }
+
+        String manufacturerForDelete = "Philips";
+
+        for (int i = 0; i < tvs.size(); i++) {
+            TV currentTV = tvs.get(i);
+            if (currentTV.getManufacturer().equals(manufacturerForDelete)) {
+                tvs.remove(currentTV);
+                //break;
+            }
+
+        }
+
+        System.out.println(" ---------- ");
+        for (int i = 0; i < tvs.size(); i++) {
+            System.out.println(tvs.get(i));
+        }
+
+
+
     }
 }
