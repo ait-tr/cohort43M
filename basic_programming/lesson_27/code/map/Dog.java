25e7e375c1f3b55fabff389ffdc981c29246a100
StanislavPB <perminovsb@gmail.com>
2024-05-17 09:59:29 +0100

    lesson 27

---
+++ basic_programming/lesson_27/code/map/Dog.java
@@ -0,0 +1,28 @@
+package code.map;
+
+public class Dog {
+
+    String name;
+    int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ basic_programming/lesson_27/code/map/MapExample3.java
@@ -0,0 +1,22 @@
+package code.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample3 {
+    public static void main(String[] args) {
+        Map<Person, Dog> map = new HashMap<>();
+
+        map.put(new Person("Ruslan",30), new Dog("Tuzik",2));
+        map.put(new Person("Oleg",35), new Dog("Bobik",3));
+        map.put(new Person("Roman",21), new Dog("Palkan",5));
+
+        System.out.println(map);
+
+        map.put(new Person("Ruslan",30), new Dog("Tuzik2", 2));
+
+        System.out.println(map);
+
+
+    }
+}
