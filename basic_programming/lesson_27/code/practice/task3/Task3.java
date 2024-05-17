7a4ae91a9fa8c07a6dce267b143308421572fde9
StanislavPB <perminovsb@gmail.com>
2024-05-17 12:00:08 +0100

    lesson 27

--- basic_programming/lesson_27/code/practice/task3/Task3.java
+++ basic_programming/lesson_27/code/practice/task3/Task3.java
@@ -1,5 +1,10 @@
 package code.practice.task3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Task3 {
     /*
     создайте коллекцию для хранения перевода слов
@@ -10,4 +15,34 @@ public class Task3 {
 
     выведите словарь на экран
      */
+
+    public static void main(String[] args) {
+        // создадим коллекцию для хранения перевода слов
+
+        List<String> englishWord = new ArrayList<>();
+        List<String> translation = new ArrayList<>();
+
+        englishWord.add("apple");
+        translation.add("яблоко");
+
+        for (int i = 0; i < englishWord.size(); i++) {
+            System.out.println("Слово " + englishWord.get(i) + " - перевод: " + translation.get(i));
+        }
+
+        // альтернативный вариант
+
+        Map<String , String > dictionary = new HashMap<>();
+
+        // задаем 5 слов с переводом
+
+        dictionary.put("apple", "яблоко");
+        dictionary.put("book", "книга");
+        dictionary.put("cat", "кошка");
+        dictionary.put("dog", "собака");
+        dictionary.put("house", "дом");
+
+        System.out.println(dictionary);
+
+    }
+
 }
