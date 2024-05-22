ebebe41314e1ecca0932d2007f660fb4a190c94f
StanislavPB <perminovsb@gmail.com>
2024-05-22 09:13:04 +0100

    lesson 29

---
+++ basic_programming/lesson_29/code/set/HashSetExample3.java
@@ -0,0 +1,27 @@
+package code.set;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class HashSetExample3 {
+    public static void main(String[] args) {
+
+        List<String> strings = new ArrayList<>();
+
+        strings.add("One");
+        strings.add("Two");
+        strings.add("Four");
+        strings.add("Ten");
+        strings.add("One");
+        strings.add("Ten");
+        strings.add("Four");
+
+
+        System.out.println(strings);
+
+
+
+    }
+}
