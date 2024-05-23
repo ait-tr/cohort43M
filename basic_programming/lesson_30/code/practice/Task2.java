e8e3f6e41d214210b66e07596f989b0af45deed3
StanislavPB <perminovsb@gmail.com>
2024-05-23 11:54:29 +0100

    lesson 30

--- basic_programming/lesson_30/code/practice/Task2.java
+++ basic_programming/lesson_30/code/practice/Task2.java
@@ -1,5 +1,9 @@
 package code.practice;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Task2 {
     /*
     Дана коллекция string
@@ -8,4 +12,38 @@ public class Task2 {
     которые содержат букву 'a' на "..."
 
      */
+
+    public static void main(String[] args) {
+
+        List<String> strings = new ArrayList<>();
+
+        strings.add("apple");
+        strings.add("banana");
+        strings.add("cherry");
+        strings.add("orange");
+        strings.add("lemon");
+        strings.add("kiwi");
+
+        System.out.println("Original list: " + strings);
+
+        replaceString(strings,"a","...");
+
+        System.out.println("Modified list: " + strings);
+
+
+
+    }
+
+    public static void replaceString(List<String> ourList, String checkContain, String replaceElement){
+        ListIterator<String> listIterator = ourList.listIterator();
+
+        while (listIterator.hasNext()){
+            String currentString = listIterator.next();
+
+            if (currentString.contains(checkContain)){
+                listIterator.set(replaceElement);
+            }
+        }
+
+    }
 }
