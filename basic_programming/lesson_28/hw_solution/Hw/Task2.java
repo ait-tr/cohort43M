460273bc278a9a3ad188b48a6c41a2b9a87cf66c
Alisa Tongaliuk <tongalise@gmail.com>
2024-05-24 18:33:47 +0000

    (HW)-user: 'Alisa_Tongaliuk-tongalise@gmail.com', commit time: '2024-05-24 18:33:47'

---
+++ basic_programming/lesson_28/hw_solution/Hw/Task2.java
@@ -0,0 +1,18 @@
+package code.Hw;
+
+import java.util.HashMap;
+
+public class Task2 {
+    public static void main(String[] args) {
+        HashMap<String, String> phonebook = new HashMap<>();
+        phonebook.put("Emma", "+7864576763977");
+        System.out.println(phonebook);
+        phonebook.put("Max", "++39565786939890");
+        System.out.println(phonebook);
+        phonebook.put("Anton", "+78048654886654");
+        System.out.println(phonebook);
+        phonebook.put("Mike", "+786457676390877");
+        System.out.println(phonebook);
+
+    }
+}
