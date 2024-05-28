283c1714a55b135b7069a964d819a9a22c231e81
StanislavPB <perminovsb@gmail.com>
2024-05-28 15:06:28 +0100

    consultation 12

---
+++ basic_programming/consultation/consultation_12/code/ContactComparator.java
@@ -0,0 +1,14 @@
+package code;
+
+import java.util.Comparator;
+
+public class ContactComparator implements Comparator<Contact> {
+    @Override
+    public int compare(Contact o1, Contact o2) {
+        if (o1.getName().compareTo(o2.getName()) == 0) {
+            return o1.getPhoneNumber().compareTo(o2.getPhoneNumber());
+        } else {
+            return o1.getName().compareTo(o2.getName());
+        }
+    }
+}
