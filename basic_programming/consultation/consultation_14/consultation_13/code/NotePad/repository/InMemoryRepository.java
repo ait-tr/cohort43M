b2f1a4d5973f33283a1d61db1929f8acb618b872
StanislavPB <perminovsb@gmail.com>
2024-06-04 15:13:58 +0100

    consultation 14

--- basic_programming/consultation/consultation_14/consultation_13/code/NotePad/repository/InMemoryRepository.java
+++ basic_programming/consultation/consultation_14/consultation_13/code/NotePad/repository/InMemoryRepository.java
@@ -8,9 +8,12 @@ import java.util.Optional;
 public interface InMemoryRepository {
     public Task add(Task newTask);
 
+    // возвращаем список всех элементов изх нашей коллекции задач
     public List<Task> findAll();
 
+    // Optional - потому что может быть задача с таким id а может и не быть найдена
     public Optional<Task> findById(Integer id);
 
+    // List - потому что name не уникальный и результатов поиска может быть несколько
     public List<Task> findByName(String name);
 }
