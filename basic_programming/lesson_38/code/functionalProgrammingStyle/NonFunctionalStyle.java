7926952c354f846da9bdd7771ff6e4e46fc95dd1
StanislavPB <perminovsb@gmail.com>
2024-06-04 12:01:18 +0100

    lesson 38

---
+++ basic_programming/lesson_38/code/functionalProgrammingStyle/NonFunctionalStyle.java
@@ -0,0 +1,16 @@
+package code.functionalProgrammingStyle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NonFunctionalStyle {
+    public static void main(String[] args) {
+        List<String> myList = new ArrayList<>();
+        changeList(myList);
+
+    }
+
+    static void changeList(List<String> list){
+        // изменял бы данные в коллекции
+    }
+}
