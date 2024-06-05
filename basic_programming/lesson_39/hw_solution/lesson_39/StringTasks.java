759a987b3c8150874d42c4064c65b6449e6e07bf
Oleksii Ilchenko <mrzygomatic@gmail.com>
2024-06-05 21:47:15 +0000

    (HW)-user: 'Oleksii_Ilchenko-mrzygomatic@gmail.com', commit time: '2024-06-05 21:47:15'

---
+++ basic_programming/lesson_39/hw_solution/lesson_39/StringTasks.java
@@ -0,0 +1,46 @@
+package lesson_39;
+
+import Lesson18hw.company.UserInput;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//1. Напишите функциональный интерфейс для преобразования строки в верхний регистр.
+//3. Напишите функциональный интерфейс для проверки, является ли строка палиндромом.
+public class StringTasks {
+    public static void main(String[] args) {
+        UserInput ui = new UserInput();
+        String text = ui.inputText("Write something to play with");
+
+        IStringOperations refUppercase = (String newInput) -> {
+            String uppercasedString = newInput.toUpperCase();
+            return uppercasedString;
+        };
+        System.out.println(toUppercase(refUppercase, text));
+
+        IIsPalindrom refPalindrom = (String newInput) -> {
+
+            String textNoSpaces = text.replace(" ", "");
+            char[] textToCharArray = textNoSpaces.toCharArray();
+            char[] reverseTextAsChar = new char[textToCharArray.length];
+
+            for (int i = 0; i < textToCharArray.length; i++) {
+                reverseTextAsChar[i] = textToCharArray[textToCharArray.length -i -1];
+            }
+
+            return Arrays.toString(textToCharArray).equalsIgnoreCase(Arrays.toString(reverseTextAsChar));
+        };
+        System.out.println(text + " is palindrom: " + isPalindrom(refPalindrom, text));
+
+    }
+    public static String toUppercase (IStringOperations ref, String string){
+        String result = ref.operationsWithString(string);
+        return result;
+    }
+    public static boolean isPalindrom (IIsPalindrom ref, String string){
+        boolean result = ref.isPalindrom(string);
+        return result;
+    }
+
+}
