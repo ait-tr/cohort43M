97f7777b5d35f498744656e92d2aefb2217c41c7
StanislavPB <perminovsb@gmail.com>
2024-06-07 11:13:15 +0100

    lesson 41

---
+++ basic_programming/lesson_41/code/practice/FlatMapExample.java
@@ -0,0 +1,42 @@
+package code.practice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FlatMapExample {
+    public static void main(String[] args) {
+
+        String[][] array = new String[][] {
+                {"Hello", "world"},
+                {"Hello", "Java"},
+                {"Hello", "student"},
+                {"Welcome","to", "java"}
+        };
+
+      //  System.out.println(Arrays.toString(array));
+
+
+        // задание - создать коллекцию List из всех уникальных слов
+
+        List<String> streamArray = Arrays.stream(array)
+                .flatMap(innerArray -> Arrays.stream(innerArray))
+                .distinct()
+                .map(s -> s.toLowerCase())
+                .sorted()
+                .toList();
+
+        System.out.println(streamArray);
+
+
+        long count = Arrays.stream(array)
+                .flatMap(Arrays::stream)
+                .count();
+
+        long countUniq = Arrays.stream(array)
+                .flatMap(Arrays::stream)
+                .distinct()
+                .count();
+
+
+    }
+}
