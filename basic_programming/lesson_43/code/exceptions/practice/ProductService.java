8a070005456a79369caf5c122b7674a6f2a39b22
StanislavPB <perminovsb@gmail.com>
2024-06-11 11:35:51 +0100

    lesson 43

---
+++ basic_programming/lesson_43/code/exceptions/practice/ProductService.java
@@ -0,0 +1,23 @@
+package code.exceptions.practice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductService {
+
+    private final ValidationService validationService;
+
+    public ProductService(ValidationService validationService) {
+        this.validationService = validationService;
+    }
+
+    public List<String> add(Product product){
+        List<String> validationResult = validationService.validate(product);
+
+        if (validationResult.isEmpty()) {
+            return Arrays.asList("Success add new product");
+        } else {
+            return validationResult;
+        }
+    }
+}
