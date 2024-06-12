6b15a0a5b7601fde688905bb353ba59a3ee7924b
StanislavPB <perminovsb@gmail.com>
2024-06-12 11:10:20 +0100

    lesson 44

---
+++ basic_programming/lesson_44/code/javaInputOutput/characterOperations/FileWriterAndReader.java
@@ -0,0 +1,29 @@
+package code.javaInputOutput.characterOperations;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterAndReader {
+    public static void main(String[] args) throws IOException {
+        writeText("basic_programming/lesson_44/code/resources/fileWriteDemo.txt","Привет! Как дела?");
+        readText("basic_programming/lesson_44/code/resources/fileWriteDemo.txt");
+
+    }
+
+    public static void writeText(String path, String text) throws IOException {
+        FileWriter writer = new FileWriter(path);
+        writer.write(text);
+        writer.close();
+    }
+
+    public static void readText(String path) throws IOException {
+        FileReader reader = new FileReader(path);
+        int character;
+
+        while ((character = reader.read()) != -1){
+            System.out.print((char) character);
+        }
+    }
+}
